b18403ba8d48e62ba916b269b7d57b59a18eefb1
Conterry <59209260+Conterry@users.noreply.github.com>
2020-08-17 15:12:03 +0300

    ...

---
+++ Assets/StartButton.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartButton : MonoBehaviour
+{
+    
+
+    public void OnClick()
+    {
+    }
+
+
+}
