68de7d97b77cac40c1caa56552e2f7d96fadca26
Conterry <59209260+Conterry@users.noreply.github.com>
2020-08-18 12:28:22 +0300

    WIP

---
+++ Assets/Game.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+
+    private bool CanStart = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (CanStart == false)
+        {
+            GameObject Button = GameObject.Find("StartButton");
+            StartButton startButton = Button.GetComponent<StartButton>();
+            CanStart = startButton.CanStartGame;
+        }
+        {
+            GameProcess();
+        }
+    }
+
+    private void GameProcess()
+    {
+
+    }
+
+
+}
--- Assets/StartButton.cs
+++ Assets/StartButton.cs
@@ -4,11 +4,29 @@ using UnityEngine;
 
 public class StartButton : MonoBehaviour
 {
-    
+
+    private Animator m_Animator;
+    public string MiracleParamName;
+    public bool CanStartGame;
+
+    public void Start()
+    {
+        CanStartGame = false;
+        m_Animator = GetComponent<Animator>();
+        TurnMiracle(false);
+
+    }
 
     public void OnClick()
     {
+        TurnMiracle(true);
+        CanStartGame = true;
     }
 
+
+    private void TurnMiracle(bool bMiracle)
+    {
+        m_Animator.SetBool(MiracleParamName, bMiracle); 
+    }
 
 }
